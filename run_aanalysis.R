c57edc956b36ac026ea11da17db4c9b95ff21fde
SrilakshmiUppalapati <40741631+SrilakshmiUppalapati@users.noreply.github.com>
2018-08-07 19:39:17 -0400

    Update run_aanalysis.R

--- run_aanalysis.R
+++ run_aanalysis.R
@@ -47,6 +47,26 @@ column.names.filtered <- grep("std\\(\\)|mean\\(\\)|activity|subject", column.na
 
 dataset.filtered <- dataset[, column.names.filtered] 
 
+# STEP #3 ******* Uses descriptive activity names to name the activities in the data set
+# STEP #4 ******* Appropriately label the data set with descriptive variable names
+
+dataset.filtered$activitylabel <- factor(dataset.filtered$activity, labels= c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+
+
+# STEP 5 ****** From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject
+
+#creating a tidy dataset with mean values for each subject and activity
+
+features.colnames = grep("std\\(\\)|mean\\(\\)", column.names, value=TRUE)
+
+dataset.melt <-melt(dataset.filtered, id = c('activitylabel', 'subject'), measure.vars = features.colnames)
+
+dataset.tidy <- dcast(dataset.melt, activitylabel + subject ~ variable, mean)
+                                   
+#creating a tidy dataset file  
+
+write.table(dataset.tidy, file = "tidydataset.txt" row.names = FALSE)
+
 
 
 
