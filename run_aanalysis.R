df2b93c8871353a17b09879f90eeba86d409a44d
SrilakshmiUppalapati <40741631+SrilakshmiUppalapati@users.noreply.github.com>
2018-08-07 20:37:29 -0400

    Update run_aanalysis.R

--- run_aanalysis.R
+++ run_aanalysis.R
@@ -13,62 +13,82 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt") #subjects
 
 features <- read.table("./UCI HAR Dataset/features.txt", as.is=TRUE)
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
-colnames(activity_labels) <- c("activityId", "activityLabel") #descriptive column names for data (STEP 4)
-
-
-
-train$activity <- y_train$V1
-
-
-
-train$subject <- factor(subject_train$V1)
-
-
-#reading test data
-
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-
-colnames(X_test) <- features$V2
-
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt") 
-
-test$activity <- y_test$V1
-
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-
-test$subject <- factor(subject_test$V1)
+colnames(activity_labels) <- c("activityId", "activityLabel") 
 
 # STEP #1 ******* Merge the training and the test sets to create one data set
 
-dataset <- rbind(X_test, X_train) 
+merged_dataset <- rbind(
+  cbind(subject_train, features_train, activity_train),
+  cbind(subject_test, features_test, activity_test)
+) 
+  # Set column names for merged data set
+colnames(merged_dataset) <- c("subject", features[, 2], "activity")
 
 # STEP #2 ******* Extract only the measurements on the mean and standard deviation for each measurement
 
-column.names <- colnames(dataset)
+  # determine columns of data set to keep based on column name...
 
-column.names.filtered <- grep("std\\(\\)|mean\\(\\)|activity|subject", column.names, value=TRUE)
+columnsToKeep <- grepl("subject|activity|mean|std", colnames(merged_dataset))
 
-dataset.filtered <- dataset[, column.names.filtered] 
+# ... and keep data in these columns only
 
-# STEP #3 ******* Uses descriptive activity names to name the activities in the data set
+merged_dataset <- merged_dataset[, columnsToKeep]
+
+# STEP #3 ******* Use descriptive activity names to name the activities in the data set
+
+# replace activity values with named factor levels
+
+merged_dataset$activity <- factor(merged_dataset$activity, 
+levels = activities[, 1], labels = activities[, 2])
+
 # STEP #4 ******* Appropriately label the data set with descriptive variable names
 
-dataset.filtered$activitylabel <- factor(dataset.filtered$activity, labels= c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+# get column names
+
+merged_dataset_columns <- colnames(merged_dataset)
+
+# remove special characters
+
+merged_dataset_columns <- gsub("[\\(\\)-]", "", merged_dataset_columns)
+
+# expand abbreviations and clean up names
+
+merged_dataset_columns <- gsub("^f", "frequencyDomain", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("^t", "timeDomain", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("Acc", "Accelerometer", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("Gyro", "Gyroscope", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("Mag", "Magnitude", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("Freq", "Frequency", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("mean", "Mean", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("std", "StandardDeviation", merged_dataset_columns)
+
+merged_dataset_columns <- gsub("BodyBody", "Body", merged_dataset_columns)
+
+# use new labels as column names
+
+colnames(merged_dataset) <- merged_dataset_columns
 
 
 # STEP 5 ****** From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject
 
 #creating a tidy dataset with mean values for each subject and activity
 
-features.colnames = grep("std\\(\\)|mean\\(\\)", column.names, value=TRUE)
+merged_dataset_means <- merged_dataset %>% 
 
-dataset.melt <-melt(dataset.filtered, id = c('activitylabel', 'subject'), measure.vars = features.colnames)
+  group_by(subject, activity) %>%
 
-dataset.tidy <- dcast(dataset.melt, activitylabel + subject ~ variable, mean)
-                                   
-#creating a tidy dataset file  
+  summarise_each(funs(mean))
 
-write.table(dataset.tidy, file = "tidydataset.txt" row.names = FALSE)
+# output the above mean values data set to a file "tidy_data.txt"
+
+write.table(merged_dataset_means, "tidy_data.txt", row.names = FALSE, quote = FALSE)
 
 
 
